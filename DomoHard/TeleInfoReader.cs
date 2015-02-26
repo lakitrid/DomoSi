cca9840f8a578707be7cdd6a3ed48009f979b4f2
eric <eric.dumortier@gmail.com>
2015-02-26 08:00:49 +0100

    Ajout du code de lecture série

---
+++ DomoHard/TeleInfoReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DomoHard
+{
+    public class TeleInfoReader
+    {
+        public void Read()
+        {
+            string filename = string.Empty;
+            int baudrate = 1200;
+
+            filename = "out.log";
+
+            SerialPort serial = null;
+            try
+            {
+                serial = new SerialPort(filename, baudrate, Parity.Even, 7, StopBits.One);
+                serial.Open();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(string.Format("Open Failed ! {0}", exc.Message));
+                return;
+            }
+
+
+            Console.WriteLine(string.Empty);
+
+            try
+            {
+                while (true)
+                {
+                    if (serial.BytesToRead > 0)
+                    {
+                        int byteCount = serial.BytesToRead;
+                        byte[] buffer = new byte[byteCount];
+
+                        serial.Read(buffer, 0, byteCount);
+
+                        //if (byteCount >= 16)
+                        //{
+                        //Console.WriteLine(string.Format("Read {0} bytes : ", byteCount));
+
+                        //foreach (byte data in buffer)
+                        //{
+                        //    Console.Write(data.ToString("X2"));
+                        //}
+
+                        //Console.WriteLine(string.Empty);
+                        foreach (byte data in buffer)
+                        {
+                            Console.Write((char)data);
+                        }
+                        //}
+                    }
+
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(string.Format("Failure while reading ! {0}", exc.Message));
+                return;
+            }
+        }
+    }
+}
---
+++ DomoRest/Log.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomoRest
+{
+    internal class Log
+    {
+        private static readonly Log InstanceObj = new Log();
+
+        private Log()
+        {
+
+        }
+
+        public static Log Instance
+        {
+            get
+            {
+                return InstanceObj;
+            }
+        }
+
+        public void Error(string message)
+        {
+            this.LogMessage("Error", message);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            this.Error(string.Format(message, args));
+        }
+
+        public void Info(string message)
+        {
+            this.LogMessage("Info", message);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            this.Info(string.Format(message, args));
+        }
+
+        private void LogMessage(string p, string message)
+        {
+
+        }
+    }
+}
