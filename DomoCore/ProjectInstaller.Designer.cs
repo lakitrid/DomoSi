bbb1a4af5451bb6630d0fb6dffc91432a38c5522
edumortier <edumortier@osaxis.fr>
2015-03-04 16:23:38 +0100

    Add missing project installer file

---
+++ DomoCore/ProjectInstaller.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Fr.Lakitrid.DomoCore
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DomoCorePInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.DomoCoreInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // DomoCorePInstaller
+            // 
+            this.DomoCorePInstaller.Password = null;
+            this.DomoCorePInstaller.Username = null;
+            // 
+            // DomoCoreInstaller
+            // 
+            this.DomoCoreInstaller.ServiceName = "DomoCoreService";
+            this.DomoCoreInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.DomoCorePInstaller,
+            this.DomoCoreInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller DomoCorePInstaller;
+        private System.ServiceProcess.ServiceInstaller DomoCoreInstaller;
+    }
+}
