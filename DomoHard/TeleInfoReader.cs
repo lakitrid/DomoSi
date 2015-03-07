33add745161c217eb0ff5d78d8816afd389a22f9
eric <eric.dumortier@gmail.com>
2015-03-07 21:44:38 +0100

    ref #1 : Add reading process for TeleInfo Data using a timer set on 1 minutes to extract 1 frame by minute Frame Decoded as TeleInfoData object.

--- DomoHard/TeleInfoReader.cs
+++ DomoHard/TeleInfoReader.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using DomoHard.Data;
 using System.IO;
+using System.ComponentModel;
 
 namespace DomoHard
 {
@@ -23,57 +24,47 @@ namespace DomoHard
         public delegate void TeleInfoDataReadHandler(TeleInfoData data);
         public event TeleInfoDataReadHandler TeleInfoDataRead;
 
+        private Timer timer;
+
         public TeleInfoReader(string serialPortName, int baudrate = 1200)
         {
+            Console.WriteLine("Start TeleInfoReader");
             this.serialPortName = serialPortName;
             this.baudrate = baudrate;
+            this.timer = new Timer(this.SerialReadCallback, null, TimeSpan.FromSeconds(5), TimeSpan.Zero);
         }
 
+        /// <summary>
+        /// Dispose TeleInfo Reader
+        /// </summary>
         public void Dispose()
         {
-            this.serialPort.Close();
+            this.timer.Dispose();
+            Console.WriteLine("Stop TeleInfoReader");
         }
 
         /// <summary>
-        /// Initialize serial port reading and subscribe to reading event
+        /// Callback of the Timer : Start a read cycle on the serial
         /// </summary>
-        /// <returns>Whether the serial port is Open</returns>
-        public bool InitSerial()
+        /// <param name="state">the timer state</param>
+        private void SerialReadCallback(object state)
         {
-            try
+            Console.WriteLine("Start TeleInfoReader SerialReadCallback");
+
+            if (this.InitSerial())
             {
-                this.serialPort = new SerialPort(this.serialPortName, this.baudrate, Parity.Even, 7, StopBits.One);
-                this.serialPort.Open();
-                this.serialPort.DataReceived += this.DataReceived;
-                return this.serialPort.IsOpen;
+                this.ReadData();
+                this.CloseSerial();
             }
-            catch (IOException exc)
-            {
-                return false;
-            }
+
+            this.timer.Change(TimeSpan.FromMinutes(1), TimeSpan.Zero);
+
+            Console.WriteLine("Stop TeleInfoReader SerialReadCallback");
         }
 
         /// <summary>
-        /// Event handler for receiving serial data
-        /// </summary>
-        /// <param name="sender">sender</param>
-        /// <param name="e">event parameters</param>
-        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            try
-            {
-                // We are reading line by line
-                TeleInfoData data = this.DecodeData(this.serialPort.ReadExisting());
-
-                if (TeleInfoDataRead != null && data != null)
-                {
-                    TeleInfoDataRead.Invoke(data);
-                }
-            }
-            catch { }
-        }
-
-        /// <summary>
+        /// Read the TeleInfo frame
+        /// 
         /// Decode TeleInfo Data Sample frame :
         /// Start with STX (0x02)
         /// Serial number :  ADCO SerialNumber C
@@ -89,18 +80,109 @@ namespace DomoHard
         /// HHPHC A ,
         /// MOTDETAT 000000 B
         /// End with ETX (0x03)
-        /// 
-        /// for each variable, start with LF, end with CR
         /// </summary>
-        /// <param name="raw">raw Data</param>
-        /// <returns></returns>
-        private TeleInfoData DecodeData(string raw)
+        private void ReadData()
         {
-            TeleInfoData data = new TeleInfoData();
+            Console.WriteLine("Start TeleInfoReader ReadData");
+            string line = this.ReadLine();
 
+            // Waiting for the first line of a frame
+            while (!line.StartsWith("ADCO"))
+            {
+                line = this.ReadLine();
+            }
 
+            // Here we are at the start of a frame, we read each lines until the next frame
+            TeleInfoData infoData = new TeleInfoData() { Date = DateTime.Now };
 
-            return data;
+            do
+            {
+                string[] datas = line.Split(new char[] { ' ' });
+
+                switch (datas[0])
+                {
+                    case "ADCO":
+                        infoData.MeterId = datas[1];
+                        break;
+                    case "ISOUSC":
+                        infoData.AccountIntensity = int.Parse(datas[1]);
+                        break;
+                    case "HCHC":
+                        infoData.LowHourCpt = decimal.Parse(datas[1]);
+                        break;
+                    case "HCHP":
+                        infoData.PeekHourCpt = decimal.Parse(datas[1]);
+                        break;
+                    case "IINST":
+                        infoData.ActualIntensity = int.Parse(datas[1]);
+                        break;
+                    case "IMAX":
+                        infoData.MaxIntensity = int.Parse(datas[1]);
+                        break;
+                    case "PAPP":
+                        infoData.ApparentPower = int.Parse(datas[1]);
+                        break;
+                    case "ADPS":
+                        infoData.HasExceed = true;
+                        break;
+                }
+
+                line = this.ReadLine();
+            }
+            while (!line.StartsWith("ADCO"));
+
+            // Launch the reading Event
+            if (this.TeleInfoDataRead != null)
+            {
+                this.TeleInfoDataRead.BeginInvoke(infoData, null, null);
+            }
+
+            Console.WriteLine("Stop TeleInfoReader ReadData");
+        }
+
+        /// <summary>
+        /// Read a line on the serial port and clean the output from special characters
+        /// </summary>
+        /// <returns>line readed</returns>
+        private string ReadLine()
+        {
+            string line = this.serialPort.ReadLine();
+            return line.Split(new char[] { (char)0x02, (char)0x03, '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        /// <summary>
+        /// Initialize serial port reading and subscribe to reading event
+        /// </summary>
+        /// <returns>Whether the serial port is Open</returns>
+        private bool InitSerial()
+        {
+            try
+            {
+                Console.WriteLine("Start TeleInfoReader InitSerial");
+                this.serialPort = new SerialPort(this.serialPortName, this.baudrate, Parity.Even, 7, StopBits.One);
+                this.serialPort.Open();
+                return this.serialPort.IsOpen;
+            }
+            catch
+            {
+                Console.WriteLine("TeleInfoReader can't open Serial port {0}", this.serialPortName);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Close Serial Port
+        /// </summary>
+        private void CloseSerial()
+        {
+            try
+            {
+                this.serialPort.Close();
+            }
+            catch
+            {
+                Console.WriteLine("TeleInfoReader can't close Serial port {0}", this.serialPortName);
+            }
         }
     }
 }
