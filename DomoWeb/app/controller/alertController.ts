2d0e6dfe5e86ad252a40b65f5e506973db2001b9
eric <eric.dumortier@gmail.com>
2015-04-29 23:05:06 +0200

    refs #6 : missing files, configure Release to generate one app.js from typescript files

---
+++ DomoWeb/app/controller/alertController.ts
@@ -0,0 +1,13 @@
+﻿module App.Controllers {
+    "use strict";
+
+    export class AlertController {
+        static $inject = ["$scope", ];
+
+        constructor(private $scope: Scope.IAlertScope) {
+            if (angular.isUndefined(this.$scope.alert)) {
+                this.$scope.alert = new Scope.AlertScope();
+            }
+        }
+    }
+} 
